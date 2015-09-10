f07c3eb55eb0be2c11af0a0f235c4df568ef5b7a
richa mittal <richa.mittal@metacube.com>
2015-09-10 18:15:41 +0530

    Added DBMS Asignment 2

--- DBMS/Assignment2/Assignment2.sql
+++
@@ -1,8 +0,0 @@
-USE library_information;
-
-SELECT i.issue_dt, i.accession_no, i.member_id,r.return_dt 
-FROM book_issue AS i 
-LEFT OUTER JOIN 
-book_return AS r 
-ON i.accession_no=r.accession_no 
-ORDER BY i.issue_dt, (SELECT member_nm FROM members WHERE i.member_id=member_id) ASC;
