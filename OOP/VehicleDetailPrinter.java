8b1555c8f3afd4806437c8fcd89aee7ca4f85669
meta-richa-mittal <richa.mittal@metacube.com>
2015-09-23 17:15:11 +0530

    Create VehicleDetailPrinter.java

---
+++ OOP/VehicleDetailPrinter.java
@@ -0,0 +1,32 @@
+package vehicle;
+
+
+/**
+ * 
+ * @author Richa Mittal
+ * Description: This class contains functions to print details of vehicles
+ * 
+ */
+public class VehicleDetailPrinter {
+	
+	
+	/**
+	 * Function will return details of the vehicle
+	 * @param vehicle: vehicle whose details are to be printed
+	 * @return details of the vehicle
+	 */
+	public String printVehicleSpecification(Vehicle vehicle) {
+		String specification = vehicle.toString();
+		return specification;
+	}
+
+	
+	/**
+	 * Function will display the string
+	 * @param data: string to be displayed
+	 */
+	public void display(String data) {
+		System.out.print(data);
+	}
+
+}
