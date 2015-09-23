9ff7ead66a530da9f67291245d2b5fd28c7d5319
meta-richa-mittal <richa.mittal@metacube.com>
2015-09-23 17:18:06 +0530

    Create VehicleHelperTest.java

---
+++ OOP/VehicleHelperTest.java
@@ -0,0 +1,28 @@
+package vehicle;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VehicleHelperTest {
+	VehicleHelper vehicleHelper=new VehicleHelper();
+	
+	@Test
+	public void testCalculateOnRoadPrice() {
+		
+		Vehicle vehicle =  vehicleHelper.createVehicle ("Bike","Royal Enfield", "Bullet 300",
+				350, 15, 30, 12000, 1500);
+		assertNotNull(vehicle);			// if vehicle is not created
+		assertEquals(15000 , vehicle.calculateOnRoadPrice());		// checks On Road price of bike
+ 	}
+	
+	@Test
+	public void testCreateCar() {
+		
+		Vehicle vehicle =  vehicleHelper.createVehicle ("Car","Ford", "Sport", 
+				2000, 50, 22, 85000, 4000);
+		assertNotNull(vehicle);			// if vehicle is not created
+		assertEquals("Sport" , vehicle.getModel());					// checks model of car
+ 	}
+
+}
