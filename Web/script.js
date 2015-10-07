ac3110cccd08376ca1c641b304c4a24317ba1b05
richa mittal <richa.mittal@metacube.com>
2015-10-07 15:45:04 +0530

    Added JS Asignment 1

--- Web/script.js
+++ Web/script.js
@@ -18,47 +18,97 @@ function activateSubmit()
 // Validation for Checking all the conditions
 function validate()
  {
+	 
 	var count=0;
 	// First name Validation
 	if(document.getElementById("firstName").value.trim() == "")
 	{
 		count=1;
 		alert("First Name Is Empty");
+		document.getElementById("firstName").style.borderColor="red";
+		document.getElementById("firstName").focus();
+		
+		
 	} 
 	// lastName Validation
 	else if(document.getElementById("lastName").value.trim() == "") 
 	{
 		count=1;
 		alert("Last Name Is Empty");
+		document.getElementById("lastName").style.borderColor="red";
+		document.getElementById("lastName").focus();
 	}
 	// Password Verfication
 	else if(document.getElementById("password").value.length < 8 )
 	{
 		count=1;
 		alert("Password Length should be greater than 8");
+		document.getElementById("password").style.borderColor="red";
+		document.getElementById("password").focus();
 	}
-	// Selecting the State and City
-	else if(document.getElementById("state") == "Select State" || document.getElementById("city").value == "Select City" ) 
+	//age verification
+	else if(document.getElementById("age").value.trim() == "")
 	{
 		count=1;
-		alert("State or City are not Selected!!");
+		alert("Age is empty");
+		document.getElementById("age").style.borderColor="red";
+		document.getElementById("age").focus();
+	}
+	// Selecting the State
+	else if(document.getElementById("state1").value == "Select State")
+	{
+		count=1;
+		alert("State is not Selected!!");
+		document.getElementById("state1").style.borderColor="red";
+		document.getElementById("state1").focus();
+	}
+	// Selecting the City
+	else if(document.getElementById("city").value == "Select City" ) 
+	{
+		count=1;
+		alert("City is not Selected!!");
+		document.getElementById("city").style.borderColor="red";
+		document.getElementById("city").focus();
 	}
 	// Matching Password and Confirm Password
 	else if(document.getElementById("password").value != document.getElementById("confirmPassword").value) 
 	{
 		count=1;
 		alert("Passwords do not match!!");
+		document.getElementById("confirmPassword").style.borderColor="red";
+		document.getElementById("confirmPassword").focus();
 	}
+	
 	
 	
 	// Checking the Conditions if user created or not
 	if(count==0)
 	{
 		alert("USER CREATED SUCCESSFULLY!!");
+		document.getElementById("firstName").value = "";
+		document.getElementById("lastName").value = "";
+		document.getElementById("password").value = "";
+		document.getElementById("confirmPassword").value = "";
+		document.getElementById("age").value = "";
+		document.getElementById("state1").value = "";
+		document.getElementById("city").value = "";
+		document.getElementById("addressLine1").value = "";
+		
+		document.getElementById("firstName").style.borderColor="initial";
+		document.getElementById("lastName").style.borderColor="initial";
+		document.getElementById("password").style.borderColor="initial";
+		document.getElementById("confirmPassword").style.borderColor="initial";
+		document.getElementById("age").style.borderColor="initial";
+		document.getElementById("state1").style.borderColor="initial";
+		document.getElementById("city").style.borderColor="initial";
+		document.getElementById("addressLine1").style.borderColor="initial";
+		
 	}
 	else 
 	{
 		alert(document.getElementById("textBox").value);
+		document.getElementById("textBox").style.borderColor="red";
+		document.getElementById("textBox").focus();
 	}
 	
 }
@@ -66,7 +116,7 @@ function validate()
 function createCity() 
 {
 	var cityObject = document.getElementById("city");
-	var stateObject = document.getElementById("state");
+	var stateObject = document.getElementById("state1");
 	switch(stateObject.value) 
 	{
 		case "Rajasthan" :
