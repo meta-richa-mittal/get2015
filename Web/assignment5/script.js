bc72754a96be787c572a48fc27d87d12f40378dc
richa mittal <richa.mittal@metacube.com>
2015-11-03 17:01:52 +0530

    Added Bootstrap Asignment 1

--- Web/assignment5/script.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $('#cover').cycle({
-       	fx:        'cover',
-       	delay:    -2000,
-		before: function(curr, next, opts) {
-			opts.animOut.opacity = 0;
-		}
-    });
-});
