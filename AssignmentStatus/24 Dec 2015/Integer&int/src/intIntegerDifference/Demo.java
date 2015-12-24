04d9045e669ceec563e15ee5acab0f9f5692708b
meta-richa-mittal <richa.mittal@metacube.com>
2015-12-24 17:48:21 +0530

    Update Demo.java

--- AssignmentStatus/24 Dec 2015/Integer&int/src/intIntegerDifference/Demo.java
+++ AssignmentStatus/24 Dec 2015/Integer&int/src/intIntegerDifference/Demo.java
@@ -5,15 +5,15 @@ public class Demo {
 	public static void main(String[] args) {
 		Integer value1 = 4;
 		int value2=6;
-		System.out.println("Value1 in main before updateInteger : "+value1);
-		System.out.println("Value2 in main before updateInteger : "+value2);
-		updateInteger(value1,value2);
-		System.out.println("Value1 in main after updateInteger : "+value1);
-		System.out.println("Value2 in main after updateInteger : "+value2);
+		System.out.println("Value1 in main before updateValue : "+value1);
+		System.out.println("Value2 in main before updateValue : "+value2);
+		updateValue(value1,value2);
+		System.out.println("Value1 in main after updateValue : "+value1);
+		System.out.println("Value2 in main after updateValue : "+value2);
 	}
 
 
-	public static void updateInteger(Integer v1, int v2) {
+	public static void updateValue(Integer v1, int v2) {
 		System.out.println("Value1 in function before update : "+v1);
 		System.out.println("Value2 in function before update : "+v2);
 		v1= 5;
