92b869758f5789459e80d0d76d91c2686c7b2e6a
meta-richa-mittal <richa.mittal@metacube.com>
2015-12-24 17:47:25 +0530

    Update Main.java

--- AssignmentStatus/24 Dec 2015/ParameterisedConstructorExample/src/parameterisedConstructor/Main.java
+++ AssignmentStatus/24 Dec 2015/ParameterisedConstructorExample/src/parameterisedConstructor/Main.java
@@ -3,7 +3,7 @@ package parameterisedConstructor;
 public class Main {
 
 	public static void main(String args[]){
-		String email = "nidhi.sharma@metacube.com";
+		String email = "richa.mittal@metacube.com";
 		Employee emp = new Employee(email);
 		System.out.println("Employee : " + emp);
 	}
